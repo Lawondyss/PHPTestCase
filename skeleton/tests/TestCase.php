016d459e76d9cfd8d55009f4a0a0c27e6f001bf2
Ladislav Vondracek <lawondyss@gmail.com>
2014-06-06 17:04:25 +0200

    Changed TestCase.
    Deleted unnecessary and uses new WebDriver.

--- skeleton/tests/TestCase.php
+++ skeleton/tests/TestCase.php
@@ -7,41 +7,22 @@
 
 namespace Tests;
 
-require_once './tests/libs/nette.min.php';
-
-class TestCase extends \PHPUnit_Framework_TestCase  
+class TestCase extends \PHPUnit_Framework_TestCase
 {
-	/** @var \WebDriver_Driver|null */
-	public $driver = NULL;
-
-	public function setUp()
-	{
-		$loader = new \Nette\Loaders\RobotLoader;
-
-		$loader->addDirectory('tests');
-		$loader->addDirectory('libs/PHPTestCase');
-
-		$loader->setCacheStorage(new \Nette\Caching\Storages\FileStorage('temp'));
-		$loader->register();
-	}
+	/** @var \RemoteWebDriver|null */
+	public $driver;
 
 
 	public function setUpSeleniumEnviroment()
 	{
-		self::setUp();
-
-		$this->driver = \WebDriver_Driver::InitAtLocal("4444", "firefox");
+		$this->driver = \RemoteWebDriver::create();
 	}
 
 
 
-	public function tearDown() {
+	public function tearDown()
+	{
 		if ($this->driver) {
-			if ($this->hasFailed()) {
-				$this->driver->set_sauce_context("passed", false);
-			} else {
-				$this->driver->set_sauce_context("passed", true);
-			}
 			$this->driver->quit();
 		}
 		parent::tearDown();
